e3896f6b92305faf4529240b211af17168c2b0d8
altisseront <altisseront@gmail.com>
2022-03-28 00:07:39 -0400

    intial commit

---
+++ likes.js
@@ -0,0 +1,27 @@
+function addLike1() {
+    var x = document.getElementById('likes1');
+    var value1 = x.innerHTML;
+
+    ++value1;
+
+    console.log(value1);
+    document.getElementById('likes1').innerHTML = value1;
+}
+function addLike2() {
+    var x = document.getElementById('likes2');
+    var value2 = x.innerHTML;
+
+    ++value2;
+
+    console.log(value2);
+    document.getElementById('likes2').innerHTML = value2;
+}
+function addLike3() {
+    var x = document.getElementById('likes3');
+    var value3 = x.innerHTML;
+
+    ++value3;
+
+    console.log(value3);
+    document.getElementById('likes3').innerHTML = value3;
+}
